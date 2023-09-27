f9f0e57d2eee5c56e996b629219088976f007c2e
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-09-27 10:59:07 +0200

    question 2.8

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -3,10 +3,11 @@ import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Button, Card, FormattedPrice, Heading, ProductCardLayout, ProductCartLine, SectionContainer } from "tp-kit/components";
 import { addLine, computeCartTotal, removeLine, updateLine, useCart } from "../../hooks/use-cart";
 import { CartData } from "../../types";
+import Cart from "../../components/cart";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 export default function DevCartPage() {
-  const lines = useCart((state) => state.lines)
+  console.log("rendu page")
   return (
     <SectionContainer
       className="py-36"
@@ -26,22 +27,7 @@ export default function DevCartPage() {
 
       {/* Panier */}
       <section className="w-full lg:w-1/3 space-y-8">
-				<Card>
-                    <Heading as="h1" size="sm" weight="bold">Mon Panier</Heading>
-                    <div className="my-16 flex gap-10 flex-col">
-                        {lines.map((line, index) => 
-                        <ProductCartLine key={index} product={line.product} qty={line.qty} onQtyChange={(qty) => {
-                          line.qty = qty
-                          updateLine(line)
-                        }} onDelete={()=> removeLine(line.product.id)} className="font-bold" />
-                        )}
-                    </div>
-                    <div className="flex justify-between font-bold mb-10">
-                        <p>Total</p>
-                        <FormattedPrice price={computeCartTotal(lines)}/>
-                    </div>
-                    <Button className="w-full">Commander</Button> 
-                </Card>
+				<Cart/>
 				<Button variant={"outline"} fullWidth>Vider le panier</Button>
 			</section>
       {/* /Panier */}
---
+++ src/components/cart.tsx
@@ -0,0 +1,24 @@
+import { Card, Heading,ProductCartLine,FormattedPrice,Button } from "tp-kit/components"
+import { updateLine,computeCartTotal,removeLine, useCart } from "../hooks/use-cart"
+
+export default function Cart(){
+    const lines = useCart((state) => state.lines)
+    return <>
+        <Card>
+                <Heading as="h1" size="sm" weight="bold">Mon Panier</Heading>
+                <div className="my-16 flex gap-10 flex-col">
+                    {lines.map((line, index) => 
+                    <ProductCartLine key={index} product={line.product} qty={line.qty} onQtyChange={(qty) => {
+                        line.qty = qty
+                        updateLine(line)
+                    }} onDelete={()=> removeLine(line.product.id)} className="font-bold" />
+                    )}
+                </div>
+                <div className="flex justify-between font-bold mb-10">
+                    <p>Total</p>
+                    <FormattedPrice price={computeCartTotal(lines)}/>
+                </div>
+                <Button className="w-full">Commander</Button> 
+        </Card>
+    </>
+}
