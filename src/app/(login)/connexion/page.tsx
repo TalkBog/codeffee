e7f23119a737b67018c26652fe0f18a13fd232b0
TalkBog <99033517+TalkBog@users.noreply.github.com>
2024-01-08 11:20:16 +0100

    question 2.8

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -1,54 +1,116 @@
-"use client"
-import { Heading, Button } from "tp-kit/components";
-import { TextInput, PasswordInput } from '@mantine/core';
+"use client";
+import {
+  Heading,
+  Button,
+  NoticeMessageData,
+  useZodI18n,
+  NoticeMessage,
+} from "tp-kit/components";
+import { TextInput, PasswordInput } from "@mantine/core";
 import Link from "next/link";
+import z from "zod";
+import { FormEvent, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useForm, zodResolver } from "@mantine/form";
+import { redirect, useRouter } from "next/navigation";
 
-export default function page (){
-    
-    return<>
-        <Heading
-        as="h1"
-        size="md"
-        variant="black"
-        weight="bold"
-        >
+const schema = z.object({
+  email: z.string().email().nonempty(),
+  password: z.string().nonempty(),
+});
+
+type schema = z.infer<typeof schema>;
+
+export default function page() {
+  const router = useRouter();
+  const [notices, setNotices] = useState<NoticeMessageData[]>([]);
+  const supabase = createClientComponentClient();
+
+  function addError() {
+    setNotices((n) => [
+      ...n,
+      { type: "error", message: "L'email ou le mot de passe est invalide" },
+    ]);
+  }
+
+  function removeNotice(index: number) {
+    setNotices((n) => {
+      delete n[index];
+      return Object.values(n);
+    });
+  }
+
+  function handleLogin(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  useZodI18n(z);
+  const form = useForm<schema>({
+    validate: zodResolver(schema),
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <>
+      <Heading as="h1" size="md" variant="black" weight="bold">
         Connexion
-        </Heading>
+      </Heading>
+      {notices.map((notice, i) => (
+        <NoticeMessage key={i} {...notice} onDismiss={() => removeNotice(i)} />
+      ))}
+      <form
+        className="flex flex-col gap-8"
+        onSubmit={form.onSubmit(async (values) => {
+          console.log("coucou");
+          const email = values.email;
+          const password = values.password;
+          const result = await supabase.auth.signInWithPassword({
+            email,
+            password,
+          });
+          if (result.error) {
+            addError();
+          } else {
+            router.refresh();
+          }
+        })}
+      >
+        <TextInput
+          variant="filled"
+          label="Adresse email"
+          withAsterisk
+          placeholder="lin.guini@barilla.it..."
+          {...form.getInputProps("email")}
+        />
 
-        <form className="flex flex-col gap-8">
-            <TextInput
-            variant="filled"
-            label="Adresse email"
-            withAsterisk
-            placeholder="lin.guini@barilla.it..."
-            />
+        <PasswordInput
+          variant="filled"
+          label="Mot de passe"
+          withAsterisk
+          placeholder="Ke$$a..."
+          {...form.getInputProps("password")}
+        />
+        <div className="flex flex-col gap-2">
+          <Button
+            fullWidth
+            onClick={function noRefCheck() {}}
+            size="lg"
+            type="submit"
+            variant="primary"
+          >
+            Se connecter
+          </Button>
 
-            <PasswordInput
-                variant="filled"
-                label="Mot de passe"
-                withAsterisk
-                placeholder="Ke$$a..."
-            />
-            <div className="flex flex-col gap-2">
-                <Button
-                fullWidth
-                onClick={function noRefCheck(){}}
-                size="lg"
-                type="submit"
-                variant="primary"
-                >
-                Se connecter
-                </Button>
-
-                <Link
-                className=" text-brand font-medium text-center p-4"
-                href={"/inscription"}
-                >
-                créer un compte
-                </Link>
-            </div>
-            
-
-        </form>
+          <Link
+            className=" p-4 text-center font-medium text-brand"
+            href={"/inscription"}
+          >
+            créer un compte
+          </Link>
+        </div>
+      </form>
     </>
+  );
 }
--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -1,19 +1,26 @@
-import { Card, SectionContainer, ZodI18nProvider } from "tp-kit/components"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Card, SectionContainer, ZodI18nProvider } from "tp-kit/components";
+import { getUser } from "../../utils/supabase";
+import { redirect } from "next/navigation";
 
-export default function layout({
-    children,
-  }: {
-    children: React.ReactNode
-  }){
-    return(
-      <SectionContainer background="coffee" >
-          <Card className="mx-[30%] my-[4%] flex flex-col">
-            <ZodI18nProvider>
-              {children}
-            </ZodI18nProvider>
-             
-          </Card>
-      </SectionContainer>
-            
-        )
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createServerComponentClient({ cookies });
+  const session = await getUser(supabase);
+  console.log(session);
+  if (session.data.session !== null) {
+    console.log("redirect");
+    redirect("/mon-compte");
+  }
+  return (
+    <SectionContainer background="coffee">
+      <Card className="mx-[30%] my-[4%] flex flex-col">
+        <ZodI18nProvider>{children}</ZodI18nProvider>
+      </Card>
+    </SectionContainer>
+  );
 }
