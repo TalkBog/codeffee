f13c91a87bebb7a136bfaf566570c2faf77c6160
TalkBog <99033517+TalkBog@users.noreply.github.com>
2024-01-15 14:00:18 +0100

    question 2.11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,47 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Lexend } from 'next/font/google'
-import { Providers } from '../components/providers';
-import { Footer } from 'tp-kit/components/footer';
-import { Menu } from '../components/menu';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
+import { Providers } from "../components/providers";
+import { Footer } from "tp-kit/components/footer";
+import { Menu } from "../components/menu";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { getUser } from "../utils/supabase";
 
 const font = Lexend({
-  subsets: ['latin'],
-  weight: ['400', '600']
+  subsets: ["latin"],
+  weight: ["400", "600"],
 });
 
 export const metadata: Metadata = {
   title: {
-    default: 'Starbucks',
-    template: '%s - Starbucks',
+    default: "Starbucks",
+    template: "%s - Starbucks",
   },
   robots: {
     index: false,
-    follow: false
-  }
-}
+    follow: false,
+  },
+};
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const supabase = createServerComponentClient({ cookies });
+  const session = await getUser(supabase);
   return (
     <html lang="fr">
       <body className={font.className}>
         <Providers font={font}>
-          <Menu />
-          
+          <Menu connected={session.data.session !== null} />
+
           {children}
         </Providers>
 
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,55 +1,76 @@
 "use client";
 
-import { FC, memo, Fragment } from "react";
+import { FC, memo, Fragment, use } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuBar, Button } from "tp-kit/components";
 import { ShoppingBag, X, User } from "@phosphor-icons/react";
 import { Cart } from "./cart";
 import { CartCounter } from "./cart-counter";
 import Link from "next/link";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { getUser } from "../utils/supabase";
+import { connected } from "process";
 
-type Props = {};
+type Props = {
+  connected: boolean;
+};
 
-const Menu: FC<Props> = memo(function () {
+const Menu: FC<Props> = memo(function ({ connected }) {
   return (
     <MenuBar
-    trailing={
-        <div className="flex flex-row items-center gap-4 justify-end">
+      trailing={
+        <div className="flex flex-row items-center justify-end gap-4">
           <Link href="/mon-compte">
-            <Button variant="ghost" className="!rounded-full !p-0 h-[44px] w-[44px] flex justify-center items-center aspect-square relative text-3xl">
+            <Button
+              variant="ghost"
+              className="relative flex aspect-square h-[44px] w-[44px] items-center justify-center !rounded-full !p-0 text-3xl"
+            >
               <User size="18" weight="bold" />
             </Button>
           </Link>
+          {connected ? (
+            <Popover as="div" className="flex justify-end">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    as={Button}
+                    variant={"ghost"}
+                    className={
+                      "relative flex aspect-square h-[44px] w-[44px] items-center justify-center !rounded-full !p-0 text-3xl"
+                    }
+                  >
+                    {open ? (
+                      <X size={18} weight="regular" />
+                    ) : (
+                      <ShoppingBag size={24} weight="regular" />
+                    )}
 
-          <Popover as="div" className="flex justify-end">
-            {({ open }) => (
-              <>
-                <Popover.Button as={Button} variant={"ghost"} className={"!rounded-full h-[44px] w-[44px] !p-0 flex justify-center items-center aspect-square relative text-3xl"}>
-                  {open 
-                    ? <X size={18} weight="regular" />
-                    : <ShoppingBag size={24} weight="regular" />}
+                    <div className="absolute -right-1 -top-1 flex aspect-square h-[20px] w-[20px] items-center justify-center rounded-full bg-brand text-center text-xs text-white">
+                      <div>
+                        <CartCounter />
+                      </div>
+                    </div>
+                  </Popover.Button>
 
-                  <div className="aspect-square bg-brand text-white text-center text-xs absolute -right-1 -top-1 rounded-full flex items-center justify-center h-[20px] w-[20px]">
-                    <div><CartCounter /></div>
-                  </div>
-                </Popover.Button>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
-                >
-                  <Popover.Panel className="absolute left-0 sm:left-auto right-0 top-full z-10 mt-6 sm:w-full sm:max-w-sm">
-                    <Cart />
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute left-0 right-0 top-full z-10 mt-6 sm:left-auto sm:w-full sm:max-w-sm">
+                      <Cart />
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          ) : (
+            ""
+          )}
         </div>
       }
     />
